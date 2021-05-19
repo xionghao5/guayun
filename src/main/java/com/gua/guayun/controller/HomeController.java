097f8d2d1de64153d693c9ec0ac6197b3fb21c1f
xionghao5 <18829037917@163.com>
2021-05-19 22:53:22 +0800

    修改

--- src/main/java/com/gua/guayun/controller/HomeController.java
+++ src/main/java/com/gua/guayun/controller/HomeController.java
@@ -1,19 +1,27 @@
 package com.gua.guayun.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @ClassName HomeController
  * @Description: TODO
  * @Author xionghao5
- * @Date 2020/8/27 
+ * @Date 2020/8/27
  * @Version V1.0
  **/
-@RestController
+@Controller
 public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "forward:/1.jpg";
+    }
+
     @GetMapping("/hello")
+    @ResponseBody
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
     }
