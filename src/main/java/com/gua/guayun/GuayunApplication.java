1a3b464492b730541f3859ed3f79eaee3b592bd0
xionghao5 <18829037917@163.com>
2021-06-21 08:20:58 +0800

    从jar换成war 在tomcat上面执行

--- src/main/java/com/gua/guayun/GuayunApplication.java
+++ src/main/java/com/gua/guayun/GuayunApplication.java
@@ -2,11 +2,19 @@ package com.gua.guayun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GuayunApplication {
+public class GuayunApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GuayunApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GuayunApplication.class);
+	}
+
 }
